1c3966a5946b834ecb80e08134bc253e181d0dd6
strongcode9527 <1477869317@qq.com>
2017-10-27 09:26:23 +0800

    :tada: weChat hello world

---
+++ src/main.js
@@ -0,0 +1,9 @@
+import Vue from 'vue'
+import App from './component/App.vue'
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#root',
+  components: { App }
+})
+
---
+++ webpack.config.js
@@ -0,0 +1,43 @@
+const path  = require('path') 
+const webpack = require('webpack')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const UglifyJSPlugin = require('uglifyjs-webpack-plugin')
+
+module.exports = {
+  entry: './src/main.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  module: {
+    rules: [
+      { 
+        test: /\.vue$/, 
+        use: 'vue-loader' ,
+      },
+      {
+        test: /\.js$/,
+        use: ['babel-loader'],
+        exclude: /node_modules/
+      }
+    ]
+  },
+  resolve: {
+    alias: {
+      'vue': 'vue/dist/vue.js'
+    }
+  },
+  devServer: {
+    compress: true,
+    port: 9000,
+    contentBase: path.join(__dirname, "dist")
+  },
+  plugins: [
+    new HtmlWebpackPlugin({template: './index.html'}),
+    new webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV: '"production"'
+      }
+    })
+  ]
+};
